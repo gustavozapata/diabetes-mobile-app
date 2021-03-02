083ae05218f30ae6cc19f077480b033605d32a82
James Newton <a>
2021-03-02 22:32:51 +0000

    [ADDED] Day, Insulin & Meal models

---
+++ client/src/model/Meal.js
@@ -0,0 +1,11 @@
+export class Meal{
+    constructor(mealID,name,carbs,fat,protein,calories,fibre){
+        this.MealID = mealID;
+        this.Name = name;
+        this.Carbs = carbs;
+        this.Fat = fat;
+        this.Protein = protein;
+        this.Calories = calories;
+        this.Fibre = fibre;
+    }
+}
