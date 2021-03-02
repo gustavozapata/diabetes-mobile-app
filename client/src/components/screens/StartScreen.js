4c353aa06d6b94ab3a6d627cb5e755df967e5bc7
Gustavo Zapata <tavordie@hotmail.com>
2021-03-02 17:21:39 +0000

    [Update] commit before installing react navigation

---
+++ client/src/components/screens/StartScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const StartScreen = () => {
+  return (
+    <View>
+      <Text>Diabetless</Text>
+    </View>
+  );
+};
+
+export default StartScreen;
