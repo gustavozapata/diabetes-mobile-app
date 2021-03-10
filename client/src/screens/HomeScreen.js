918974db0577b59e779cc4c8b3ca275a88cb23f3
James Newton <a>
2021-03-10 17:09:06 +0000

    [ADDED] navigation containers to Home & Insulin screens

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -1,53 +1,59 @@
 import React from "react";
-import { View, Text, StatusBar } from "react-native";
+import { View, Text, StatusBar, Button } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from '@react-navigation/native';
 
-import {ProfileComponent} from "../components/Profile";
-import {EditProfileComponent} from "../components/ProfileEdit";
+import { ProfileComponent } from "../components/ProfileComponent";
+import { EditProfileComponent } from "../components/ProfileEditComponent";
 
 const HomeScreen = () => {
   const Stack = createStackNavigator();
 
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={MainScreen} />
-      <Stack.Screen name="Profile" component={ProfileComponent} />
-      <Stack.Screen name="EditProfile" component={EditProfileComponent} />
-    </Stack.Navigator>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={MainScreen} />
+        <Stack.Screen name="Profile" component={ProfileComponent} />
+        <Stack.Screen name="EditProfile" component={EditProfileComponent} />
+        <Stack.Screen name="Export" component={ExportComponent} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
-const MainScreen = ({props}) => {
+const MainScreen = ({ props }) => {
   return (
+    <View>
+      <StatusBar style="auto" />
+      <Text>Dashboard</Text>
       <View>
-        <StatusBar style="auto" />
-        <Text>Dashboard</Text>
-        <View>
-          <Text>Daily Average</Text>
-        </View>
-        <View>
-          <Text>Calories</Text>
-          <Text></Text>
-        </View>
-        <View>
-          <Text>Carbohydrates</Text>
-          <Text></Text>
-        </View>
-        <View>
-          <Text>Protein</Text>
-          <Text></Text>
-        </View>
-        <View>
-          <Text>Fat</Text>
-          <Text></Text>
-        </View>
-        <View>
-          <Text>Insulin usage</Text>
-        </View>
-        <View>
-          <Text>Edit profile</Text>
-        </View>
+        <Text>Daily Average</Text>
       </View>
+      <View>
+        <Text>Calories</Text>
+        <Text></Text>
+      </View>
+      <View>
+        <Text>Carbohydrates</Text>
+        <Text></Text>
+      </View>
+      <View>
+        <Text>Protein</Text>
+        <Text></Text>
+      </View>
+      <View>
+        <Text>Fat</Text>
+        <Text></Text>
+      </View>
+      <View>
+        <Text>Insulin usage</Text>
+      </View>
+      <View>
+        <Text>Edit profile</Text>
+      </View>
+      <Button title="Profile" />
+      <Button title="Save Export" />
+    </View>
   );
 };
 
--- client/src/screens/InsulinScreen.js
+++ client/src/screens/InsulinScreen.js
@@ -1,23 +1,35 @@
 import React, { useState, useEffect } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { View, Text, Button, TextInput } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+
+import { InsulinEnterComponent } from "../components/InsulinEnterComponent";
+import { InsulinDeleteComponent } from "../components/InsulinDeleteComponent";
 
 const Stack = createStackNavigator();
 
 const InsulinScreen = () => {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Insulin" component={InsulinMainComponent} />
-      <Stack.Screen name="InsulinEnter" component={InsulinEnterComponent} />
-    </Stack.Navigator>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Insulin">
+        <Stack.Screen name="Insulin" component={InsulinMainComponent} />
+        <Stack.Screen name="InsulinEnter" component={InsulinEnterComponent} />
+        <Stack.Screen name="InsulinDelete" component={InsulinDeleteComponent} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
 
 const InsulinMainComponent = ({ navigation, route }) => {
   return (<View>
     <Text>Main screen</Text>
+    <Button title="" onPress={() => { navigation.navigate("InsulinEnter")}}/>
+    <View>
+      <Text></Text>
+      <Button title="" onPress={() => { navigation.navigate("InsulinDelete")}}/>
+    </View>
     <Text>Insulin graph goes here</Text>
-    </View>);
+  </View>);
 };
 
 export default InsulinScreen;
